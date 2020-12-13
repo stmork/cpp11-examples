3e937c8f9c750d17a043ac589b53291114959a04
Steffen A. Mork <linux-dev@morknet.de>
2020-12-13 15:26:22 +0000

    * More enum stuff.

---
+++ enum.cc
@@ -0,0 +1,42 @@
+/**
+ * $Id$
+ * $Revision$
+ * $Date$
+ * $Author$
+ * @brief   Simple hash implementation.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <strings.h>
+
+enum Unscoped : char
+{
+	ZERO, ONE, TWO, THREE, MINUS_ONE = -1
+};
+
+enum struct Scoped : unsigned
+{
+	ZERO, ONE, TWO, THREE
+};
+
+int main()
+{
+	Scoped sd0   = Scoped::ZERO;
+	Scoped sd2   = Scoped::TWO;
+	Unscoped ud0 = Unscoped::ZERO;
+	Unscoped ud2 = Unscoped::TWO;
+
+	unsigned uu0 = ud0;
+	unsigned uu2 = ud2;
+	unsigned uum = Unscoped::MINUS_ONE;
+	signed   us0 = ud0;
+	signed   us2 = ud2;
+	signed   usm = Unscoped::MINUS_ONE;
+	unsigned su0 = (unsigned) sd0;
+	unsigned su2 = (unsigned) sd2;
+
+	printf("Enums: %u %u %u\n", uu0, uu2, uum);
+	printf("Enums: %d %d %d\n", us0, us2, usm);
+	printf("Enums: %u %u\n", su0, su2);
+}
