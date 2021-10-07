b02b48cae5c11a6909ee21a186af15442e80fc13
Steffen A. Mork <linux-dev@morknet.de>
2021-10-07 10:36:55 +0000

    * Beispiel für Condition Variable."

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 CXXFLAGS += -Wall -Wextra -fstack-protector-strong -std=c++17
 CXXFLAGS += -O3
 SRC       = container.cc set.cc exception.cc regex.cc constructor.cc hash.cc\
-	mapper.cc sharedptr.cc units.cc singleton.cc string.cc enum.cc rsqrt.cc
+	mapper.cc sharedptr.cc units.cc singleton.cc string.cc enum.cc rsqrt.cc cv.cc
 PRGS      = container set exception regex constructor hash mapper\
-	sharedptr units singleton string enum rsqrt
+	sharedptr units singleton string enum rsqrt cv
 
 all:	$(PRGS)
 
@@ -48,5 +48,8 @@ enum:	enum.o
 
 rsqrt:	rsqrt.o
 	$(LINK.cc) -o $@ $^
+
+cv:	cv.o
+	$(LINK.cc) -o $@ $^ -lpthread
 
 -include .dep
---
+++ cv.cc
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+int main()
+{
+	std::mutex              mutex;
+	std::condition_variable cv;
+	std::thread             thread([&]()
+	{
+		printf("T> Thread start with locking.\n");
+		std::unique_lock<std::mutex> lock(mutex);
+
+		printf("T> Waiting for notification...\n");
+		cv.wait(lock);
+		printf("T> End.\n");
+	});
+
+	printf("M> Waiting...\n");
+	sleep(2);
+	printf("M> Notifying...\n");
+	cv.notify_all();
+	printf("M> Joining...\n");
+	thread.join();
+	printf("M> End.\n");
+
+	return EXIT_SUCCESS;
+}
